c9e7849c561dd2fa280711cbd34f4db294502e04
Tristan Barnes <Tristan@Barnes.pro>
2017-10-14 19:37:54 -0400

    Initial Commit

---
+++ Fibor/Fibor.h
@@ -0,0 +1,19 @@
+//
+//  Fibor.h
+//  Fibor
+//
+//  Created by Tristan Barnes on 10/14/17.
+//  Copyright © 2017 Tristan Barnes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Fibor.
+FOUNDATION_EXPORT double FiborVersionNumber;
+
+//! Project version string for Fibor.
+FOUNDATION_EXPORT const unsigned char FiborVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Fibor/PublicHeader.h>
+
+
